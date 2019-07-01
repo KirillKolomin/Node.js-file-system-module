9884ebbf7242c3e6d07d8f4924e6dd4671102c18
Kirill.Kolomin <kirill.kolomin@sidenis.com>
2019-07-01 12:05:49 +0700

    Use streaming reading methods

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer();
+const server = http.createServer((req, res)=>{
+    const stream = fs.createReadStream('text.txt');
+    stream.pipe(res);
+});
 
 fs.readFile('text.txt', (err, data)=>{
     if (err) {
-        return callback(err)
+        return callback(err);
     }
     console.log(data, 'async');
 });
@@ -16,4 +19,3 @@ const data = fs.readFileSync('text.txt', "utf8");
 console.log(data, 'sync');
 
 server.listen(port, hostname);
-
